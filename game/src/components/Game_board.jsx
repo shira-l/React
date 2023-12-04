3cbb47b33427ff2ca7fd4ef767b68c2b24a43f2e
shira-l <Hand4c14@mbjcomp.org.il>
2023-12-04 14:44:05 +0200

    monday

--- game/src/components/Game_board.jsx
+++ game/src/components/Game_board.jsx
@@ -1,9 +1,32 @@
-import Log_in from "./Log_in"
-
+import Player_board from './Player_board'
+class Player {
+    constructor(name) {
+        this.name = name;
+        this.steps=0;
+        this.number=Math.floor(Math.random() * 99);
+        this.score=[];
+        this.average=0;
+    }
+}
+function Add_Player(players) {
+    let name;
+    do {
+        name=prompt("Please enter your name");
+    } while (name=='');
+    
+    
+    players.push(new Player(name))
+       
+       
+ 
+         
+ }
 function Game_board(){
+    let players=[]
     return(
         <>
-        <Log_in/>
+        <button onClick={()=>Add_Player(players)}>add gamer</button>
+        <Player_board players={players}/>
         </>
     )
 }
